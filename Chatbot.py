8a67c50b934e42c32b7899d89a54ffb744825d09
codingalvin <alvin.universe@gmail.com>
2025-11-09 20:39:22 -0500

    Add files via upload

--- Chatbot.py
+++ Chatbot.py
@@ -1,100 +1,10 @@
+#import libraries, set up ANSI escape and varible 
 import random
 import time
 from datetime import datetime
 BOLD = '\033[1m'
 END = '\033[0m'
-
-
-
-def greeting():
-    def greetingprinting():
-        greetingvocab=["Hi","Hello", "What's up","How are you doing" ]
-        randomgreeting = random.choice(BOLD+greetingvocab+END)
-        print(randomgreeting)
-    
-    if "hi" in userinput:
-        greetingprinting()
-    elif "Hi" in userinput:
-        greetingprinting()
-    elif "Hello" in userinput:
-        greetingprinting()
-    elif "hello" in userinput:
-        greetingprinting()
-    elif "Hey" in userinput:
-        greetingprinting()
-    elif "hey" in userinput:
-        greetingprinting()
-
-
-def clock():
-    def clockprint():
-        now = datetime.now()
-        currenttime = now.strftime("%H:%M:%S")
-        print("It is currently", currenttime ,".")
-
-    if "time" in userinput:
-        clockprint()
-    elif "Time" in userinput:
-        clockprint()
-
-def who():
-    who=["I am Gary, your personal assistant. ","This is Gary, your pal. "]
-    if "you" in userinput:
-        randomwho = random.choice(who)
-        print(BOLD+randomwho+END)
-
-    if "You" in userinput:
-        randomwho = random.choice(who)
-        print(BOLD+randomwho+END)
-
-def joke():
-    def jokeprinting():
-        jokesquestions=["Why was Cinderealla so bad at Soccer?","What do you call a fish without an eye?"]
-        jokesanswers=["She kept running away from the ball!","Fsh."]
-        randomjokenumber=random.randint(0,1)
-        randomjokequestion=jokesquestions[randomjokenumber]
-        randomjokeanswer=jokesanswers[randomjokenumber]
-        print(BOLD+randomjokequestion+END)
-        time.sleep(2.5)
-        print()
-        print(BOLD+randomjokeanswer," Haha!"+END)
-
-    if "joke" in userinput:
-        jokeprinting()
-    if "Joke" in userinput:
-        jokeprinting()
-    if "Jokes" in userinput:
-        jokeprinting()
-    if "jokes" in userinput:
-        jokeprinting()
-        
-
-def feelings():
-    def feelingsprinting():
-        feelingvocab=["I am good","I am feeling wonderful", "I am doing amazing" ]
-        feelingvocab2=["thanks", "how about you","hope your doing well too."]
-        randomfeeling1 = random.choice(feelingvocab)
-        randomfeeling2 = random.choice(feelingvocab2)
-        print(BOLD + randomfeeling1,", ",randomfeeling2 + END)
-
-    if "Doing" in userinput:
-        feelingsprinting()
-        
-    if "doing" in userinput:
-        feelingsprinting()
-        
-    if "feeling" in userinput:
-        feelingsprinting()
-        
-    if "Feeling" in userinput:
-        feelingsprinting()
-        
-    if "how are you" in userinput:
-        feelingsprinting()
-        
-    if "How are you"in userinput:
-        feelingsprinting()
-        
+go=True
 
 def iam():
     splitedinput=""
@@ -115,25 +25,111 @@ def iam():
         splitedinput.remove("is")
     print(BOLD+splitedinput+END)
         
- 
-
-
-
+#startup phrase
 startgreetings=["Hello, this is Gary. Feel free to ask anything. ", "Hi!, How can I help you today? ","Welcome!, what can I do for you today? "]
 randomgreet = random.choice(startgreetings)
-
 print(BOLD+randomgreet+END)
 
-while True:
-    userinput=input("Type here")
-    greeting()
-    clock()
-    who()
-    joke()
-    feelings()
+while go==True:
+#var setup
+    output=""
+    itisajoke=False
+#greetings
+    userinput=input()
+    def greetingprinting():
+        greetingvocab=["Hi","Hello", "What's up","How are you doing" ]
+        randomgreeting = random.choice(greetingvocab)
+        output=randomgreeting
+        return output
+    
+    if "hi" in userinput:
+        output=greetingprinting()
+    elif "Hi" in userinput:
+        output=greetingprinting()
+    elif "Hello" in userinput:
+        output=greetingprinting()
+    elif "hello" in userinput:
+        output=greetingprinting()
+    elif "Hey" in userinput:
+        output=greetingprinting()
+    elif "hey" in userinput:
+        output=greetingprinting()
+
+#clock
+    def clockprint():
+        now = datetime.now()
+        currenttime = now.strftime("%H:%M:%S")
+        output="It is currently"+currenttime +"."
+        return output
+
+    if "time" in userinput:
+        output=clockprint()
+    elif "Time" in userinput:
+        output=clockprint()
+
+#feeling
+    def feelingsprinting():
+        feelingvocab=["I am good","I am feeling wonderful", "I am doing amazing" ]
+        feelingvocab2=["thanks", "how about you","hope your doing well too."]
+        randomfeeling1 = random.choice(feelingvocab)
+        randomfeeling2 = random.choice(feelingvocab2)
+        output=randomfeeling1,", ",randomfeeling2
+        return output
+
+    if "Doing" in userinput:
+        output=feelingsprinting()
+    if "doing" in userinput:
+        output=feelingsprinting()
+    if "feeling" in userinput:
+        output=feelingsprinting()
+    if "Feeling" in userinput:
+        output=feelingsprinting()
+    if "how are you" in userinput:
+        output=feelingsprinting()
+    if "How are you"in userinput:
+        output=feelingsprinting()   
+#who
+    who=["I am Gary, your personal assistant. ","This is Gary, your pal. "]
+    if "you" in userinput:
+        randomwho = random.choice(who)
+        output=randomwho
+
+    if "You" in userinput:
+        randomwho = random.choice(who)
+        output=randomwho
  
+#leave
+    checkleavelist=0
+    leavelist=["leave","exit","sleep","goodbye","bye","Bye","Sleep","shut","Shut"]
+    for x in leavelist:
+        checkleave=leavelist[checkleavelist]
+        if checkleave in userinput:
+            output="Bye and have a nice day!"
+            go=False
+        checkleavelist=checkleavelist+1
 
+#joke
+    if "joke" in userinput:
+        itisajoke=True
+    if "Joke" in userinput:
+        itisajoke=True
+    if "Jokes" in userinput:
+        itisajoke=True
+    if "jokes" in userinput:
+        itisajoke=True
 
-
-
-
+    if itisajoke==True:
+        jokesquestions=["Why was Cinderealla so bad at Soccer?","What do you call a fish without an eye?"]
+        jokesanswers=["She kept running away from the ball!","Fsh."]
+        randomjokenumber=random.randint(0,1)
+        randomjokequestion=jokesquestions[randomjokenumber]
+        randomjokeanswer=jokesanswers[randomjokenumber]
+        print(randomjokequestion)
+        time.sleep(2.5)
+        print()
+        print(randomjokeanswer+" Haha!")
+    elif output=="":
+        print("Sorry the message that you inputed was not decected,pls rty daganin")
+        
+    if itisajoke==False:
+        print(BOLD+output+END)
